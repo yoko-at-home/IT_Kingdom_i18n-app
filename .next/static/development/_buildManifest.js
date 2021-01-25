842e833b76db17bbdc44b77f2af146a4f01be7ca
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-01-25 17:46:20 +0900

    .next追加してみた

---
+++ .next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/gsp/[slug]":["static\u002Fchunks\u002Fpages\u002Fgsp\u002F[slug].js"],sortedPages:["\u002F","\u002F_app","\u002F_error","\u002Fgsp\u002F[slug]"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
